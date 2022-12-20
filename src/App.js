2d11da6743e8d78a4cb44e58debfbd8adefae491
Jakub Borowy <borowyjakub32@gmail.com>
2022-12-20 16:28:54 +0100

    Rendering 5 random cards on click

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Board from './components/Board/Board';
 import Cards from './components/Cards/Cards'
+import AddCard from "./components/Cards/AddCard";
 import allCards from './allCards.js';
 
-import styles from './../src/App.module.css'
+import styles from './../src/App.module.css';
 
 const App = () => {
 
-  const [selectedCards, setSelectedCards] = useState([...allCards])
+  const [selectedCards, setSelectedCards] = useState([])
+  const [runSelectRandomCards, setRunSelectRandomCards] = useState(false)
+
+  // select 5 random cards from the board
+
+  const selectRandomCards = () => {
+    const cards = [...allCards];
+    const selected = [];
+    while (selected.length < 5) {
+      const index = Math.floor(Math.random() * cards.length);
+      selected.push(cards[index]);
+      cards.splice(index, 1);
+    }
+    setSelectedCards(selected);
+  };
+
+  // run after click addCard button
+
+  useEffect(() => {
+    if (runSelectRandomCards) {
+      selectRandomCards()
+    }
+  }, [runSelectRandomCards])
+
+
+  // Add 5 cards to the game table
+
+  const addCard = () => {
+    setRunSelectRandomCards(true)
+
+
+  };
+
   return (
     <>
       <section className={styles.section} onContextMenu={(e) => e.preventDefault()}>
         <h1>Okey Card Game</h1>
         <Board />
+        <AddCard addCard={addCard} />
         <div className={styles.cards}>
           {selectedCards.map((card) => {
             return (
